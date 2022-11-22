59a77195b5eae8fbc8e302bbda62f97c6e9b9632
Kovács Gergely <kovacs.gergely1@students.jedlik.eu>
2022-11-22 09:26:21 +0100

    Menu definialasa

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
 #Functions
+
+def menu():
+    valasztott=''
+    print('-----------PÉKSÉG-----------')
+    print('0 - Kilépés')
+    print('1 - Termékek kiírása')
+    print('2 - Termékek és árak kiírása')
+    print('3 - Termékek darabszámának kiírása')
+    print('4 - Új termék felvétele')
+    print('5 - Termék törlése')
+    print('6 - Ár módosítása')
+    print('7 - Mentés fájlba')
+    valasztott=input('Válasszon egy menüpontot: ')
+    return valasztott
